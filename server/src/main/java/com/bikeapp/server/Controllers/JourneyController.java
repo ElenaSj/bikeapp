aef260d57187b1f1a0a92dce882f7a79356f1e45
ElenaSj <elena.sjoblom@gmail.com>
2023-05-04 15:53:38 +0300

    controller to get all journeys w pagination

--- server/src/main/java/com/bikeapp/server/Controllers/JourneyController.java
+++ server/src/main/java/com/bikeapp/server/Controllers/JourneyController.java
@@ -1,5 +1,36 @@
 package com.bikeapp.server.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bikeapp.server.Repositories.JourneyRepository;
+import com.bikeapp.server.Entities.Journey;
+
+@RestController
+@RequestMapping("/api")
 public class JourneyController {
+	
+	@Autowired
+	JourneyRepository repo;
+	
+	@GetMapping("/journeys")
+	public List<Journey> getAll(
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "50") int size){
+		List<Journey> journeys = new ArrayList<>();
+		Pageable paging = PageRequest.of(page, size);
+		Page<Journey> pj = repo.findAll(paging);
+		journeys = pj.getContent();
+		return journeys;
+	}
 
 }
--- server/src/main/java/com/bikeapp/server/Repositories/JourneyRepository.java
+++ server/src/main/java/com/bikeapp/server/Repositories/JourneyRepository.java
@@ -1,9 +1,12 @@
 package com.bikeapp.server.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bikeapp.server.Entities.Journey;
 
 public interface JourneyRepository extends JpaRepository<Journey,Integer> {
+
 
 }
