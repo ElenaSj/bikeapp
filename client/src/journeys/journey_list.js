3a6b711be447615bf4b2b406e4c2052cecc0b3ae
ElenaSj <elena.sjoblom@gmail.com>
2023-05-05 21:18:36 +0300

    pagination for journeylist

--- client/src/journeys/journey_list.js
+++ client/src/journeys/journey_list.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import './journey_list.css'
 
 const Journeys = ({ journeys }) => {
     let journeyrows = journeys.map(j => {
@@ -14,7 +15,8 @@ const Journeys = ({ journeys }) => {
     })
     
     return (
-        <>
+        <div className="col-9">
+            <div className="journeytable">
             <table className="table table-striped">
                 <thead>
                     <tr>
@@ -24,26 +26,38 @@ const Journeys = ({ journeys }) => {
                         <th scope="col">Distance (kilometers)</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="tablerows">
                     { journeyrows }
                 </tbody>
             </table>
-        </>
+            </div>
+        </div>
     )
 }
 
 const JourneyList = () => {
     const [journeys, getJourneys] = useState([])
+    const [page, setPage] = useState(0)
 
     useEffect(() => {
-        axios.get('/api/journeys')
+        axios.get('/api/journeys?page='+page)
             .then(response => getJourneys(response.data))
-    },[])
+    },[page])
 
     return (
-        <div>
+        <div className="row journeys">
             <h2>Bike journeys</h2>
+            <nav>
+                <ul className="pagination">
+                    <li onClick={()=>setPage(page-1)} className="page item"><button className="page-link">Previous</button></li>
+                    <li className="page item active"><button className="page-link">{page+1}</button></li>
+                    <li onClick={()=>setPage(page+1)} className="page item"><button className="page-link">Next</button></li>
+                </ul>
+            </nav>
             <Journeys journeys={journeys} />
+            <div className="col-3">
+                <h2>Searcing functionality could be here</h2>
+            </div>
         </div>
     )
 
