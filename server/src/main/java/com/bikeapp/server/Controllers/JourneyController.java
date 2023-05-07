93e44eb90f5eb4b84204d4ddddd25536c6906617
ElenaSj <elena.sjoblom@gmail.com>
2023-05-07 13:52:30 +0300

    changed journeycontroller to return a response entity w number of pages

--- server/src/main/java/com/bikeapp/server/Controllers/JourneyController.java
+++ server/src/main/java/com/bikeapp/server/Controllers/JourneyController.java
@@ -1,12 +1,16 @@
 package com.bikeapp.server.Controllers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,20 +27,36 @@ public class JourneyController {
 	JourneyRepository repo;
 	
 	@GetMapping("/journeys")
-	public List<Journey> getAll(
+	public ResponseEntity<Map<String, Object>> getAll(
 			@RequestParam(required = false) String station,
 			@RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "50") int size){
-		List<Journey> journeys = new ArrayList<>();
-		Pageable paging = PageRequest.of(page, size);
-		Page<Journey> pj;
-		if (station==null || station.isEmpty()) {
-			pj=repo.findAll(paging);
-		} else {
-			pj=repo.findByDepartureStationContainingOrReturnStationContaining(station, station, paging);
+		try {
+			Pageable paging = PageRequest.of(page, size);
+			Page<Journey> pj;
+		
+			if (station==null || station.isEmpty()) {
+				pj=repo.findAll(paging);
+			} else {
+				pj=repo.findByDepartureStationContainingOrReturnStationContaining(station, station, paging);
+			} 
+	
+			Map<String, Object> response = new HashMap<>();
+		
+			response.put("journeys", pj.getContent());
+			response.put("totalPages", pj.getTotalPages());
+		
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		
+		} catch (Exception ex) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		journeys = pj.getContent();
-		return journeys;
+		
+		
+		
+		
+		
+		
 	}
 
 }
